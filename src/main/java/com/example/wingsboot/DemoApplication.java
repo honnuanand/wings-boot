70b88a13c4924476e0f702adf14755d428831e02
Anand Rao <anandrao@Anands-MBP.local>
2017-11-08 10:35:35 -0500

    initial

---
+++ src/main/java/com/example/wingsboot/DemoApplication.java
@@ -0,0 +1,21 @@
+package com.example.wingsboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@RequestMapping( method = RequestMethod.GET)
+	public String hello() {
+		return "Hello World";
+	}
+}
