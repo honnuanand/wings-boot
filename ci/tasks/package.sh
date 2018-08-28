59692829e13c5198ecaadd36d95b2ee6d78ecf73
honnuanand <anand@honnu.com>
2018-08-27 17:45:33 -0700

    moved most of the parameters  into params files

--- ci/tasks/package.sh
+++ ci/tasks/package.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
 set -e -u -x
-
 cd source-code/
-echo `pwd`
 ./mvnw package
-
-mv target/*.jar ../app-jar
-cp manifest.yml ../app-jar
-ls ../app-jar
+#mv target/*.jar ../app-jar
+#cp manifest.yml ../app-jar
+#ls ../app-jar
